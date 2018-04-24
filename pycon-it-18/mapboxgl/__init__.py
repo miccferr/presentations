d96b6ecadbb13ffc9213715e3700855a7ef170a1
Michele Ferretti <mic.ferretti@gmail.com>
2018-04-24 12:04:57 +0200

    add mpx library

---
+++ pycon-it-18/mapboxgl/__init__.py
@@ -0,0 +1,6 @@
+from .viz import CircleViz, GraduatedCircleViz, HeatmapViz, ClusteredCircleViz, ImageViz, RasterTilesViz, ChoroplethViz
+from .utils import create_color_stops
+from .class_intervals import jenks
+
+__version__ = "0.6.0"
+__all__ = ['CircleViz', 'GraduatedCircleViz', 'HeatmapViz', 'ClusteredCircleViz', 'ImageViz', 'RasterTilesViz', 'ChoroplethViz']
